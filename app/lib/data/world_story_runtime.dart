13f4d8b45c3c2982691b0a197fda67718797abcd
dhv5bnmfmh-cmyk <dhv5bnmfmh@privaterelay.appleid.com>
2026-07-20 05:46:45 +0700

    feat: publish the daily journey catalog

--- app/lib/data/world_story_runtime.dart
+++ app/lib/data/world_story_runtime.dart
@@ -1,13 +1,13 @@
 import '../agents/phoenix_world_story_agent.dart';
 import '../models/story_content.dart';
-import 'beijing_story_catalog.dart';
+import 'daily_journey_catalog.dart';
 import 'world_geo_catalog.dart';
 
 PhoenixWorldStoryAgent createPhoenixWorldStoryAgent() {
   return PhoenixWorldStoryAgent(
     nodes: worldGeoCatalog,
-    sources: beijingStorySources,
-    journeys: beijingJourneyCatalog,
+    sources: dailyStorySources,
+    journeys: dailyJourneyRecords,
   );
 }
 
