8f883a90e0943eb27a2ca3ce5ae361e665462242
dhv5bnmfmh-cmyk <dhv5bnmfmh@privaterelay.appleid.com>
2026-07-24 22:40:40 +0700

    test: require two balanced Summer Palace story and discovery paragraphs

--- app/test/summer_palace_standard_story_test.dart
+++ app/test/summer_palace_standard_story_test.dart
@@ -2,12 +2,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:phoenix_journeys/data/summer_palace_journey.dart';
 
 void main() {
-  test('Summer Palace uses one continuous longer story article', () {
-    expect(summerPalaceStoryParagraphs, hasLength(1));
-    expect(summerPalaceStoryAnnotations, hasLength(1));
+  test('Summer Palace uses two balanced long story paragraphs', () {
+    expect(summerPalaceStoryParagraphs, hasLength(2));
+    expect(summerPalaceStoryAnnotations, hasLength(2));
+    for (final paragraph in summerPalaceStoryParagraphs) {
+      expect(paragraph.length, inInclusiveRange(260, 380));
+    }
     expect(
-      summerPalaceStoryParagraphs.single.length,
-      inInclusiveRange(600, 1300),
+      summerPalaceStoryParagraphs.join().length,
+      inInclusiveRange(600, 760),
     );
+  });
+
+  test('Summer Palace discovery mirrors the two-paragraph story rhythm', () {
+    expect(summerPalaceDiscoveries, hasLength(2));
+    for (final discovery in summerPalaceDiscoveries) {
+      expect(discovery.text.length, inInclusiveRange(240, 340));
+    }
   });
 }
