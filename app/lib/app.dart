54353b7493d46ca29a7bb962bdfe86f7f7e0904e
dhv5bnmfmh-cmyk <dhv5bnmfmh@privaterelay.appleid.com>
2026-07-25 19:41:38 +0700

    feat: expose lost scroll prototype route

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'screens/lost_scroll_prototype_screen.dart';
 import 'theme/phoenix_theme.dart';
 import 'widgets/startup_gate.dart';
 
@@ -8,11 +9,14 @@ class PhoenixApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final prototype = Uri.base.queryParameters['prototype'];
     return MaterialApp(
       title: 'Phoenix Journeys',
       debugShowCheckedModeBanner: false,
       theme: PhoenixTheme.light,
-      home: const StartupGate(),
+      home: prototype == 'lost-scroll'
+          ? const LostScrollPrototypeScreen()
+          : const StartupGate(),
     );
   }
 }
