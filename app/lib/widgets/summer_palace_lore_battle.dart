16c6d03dfce770063da71b1f7e67ff9ed5cff0d2
dhv5bnmfmh-cmyk <dhv5bnmfmh@privaterelay.appleid.com>
2026-07-25 19:04:07 +0700

    feat: connect saved journey words to wuxia battle

--- app/lib/widgets/summer_palace_lore_battle.dart
+++ app/lib/widgets/summer_palace_lore_battle.dart
@@ -1,1 +1,39 @@
-export 'summer_palace_journey_arsenal.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../state/app_state.dart';
+import 'summer_palace_journey_arsenal.dart' as arsenal;
+
+class SummerPalaceLoreBattle extends StatelessWidget {
+  const SummerPalaceLoreBattle({
+    super.key,
+    required this.onCompleted,
+    this.completed = false,
+    this.scenarioSeed,
+    this.ruleSeed,
+    this.learnedWords = const <String>{},
+  });
+
+  final VoidCallback onCompleted;
+  final bool completed;
+  final int? scenarioSeed;
+  final int? ruleSeed;
+  final Set<String> learnedWords;
+
+  @override
+  Widget build(BuildContext context) {
+    final appState = Provider.of<AppState?>(context, listen: false);
+    final availableKnowledge = <String>{
+      ...?appState?.savedWords,
+      ...learnedWords,
+    };
+
+    return arsenal.SummerPalaceLoreBattle(
+      completed: completed,
+      scenarioSeed: scenarioSeed,
+      ruleSeed: ruleSeed,
+      learnedWords: availableKnowledge,
+      onCompleted: onCompleted,
+    );
+  }
+}
