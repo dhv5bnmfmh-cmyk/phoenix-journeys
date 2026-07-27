f042036c2d893bf8fd41468cad41d2e53a6a76ba
dhv5bnmfmh-cmyk <dhv5bnmfmh@privaterelay.appleid.com>
2026-07-28 04:37:48 +0700

    fix: remove obsolete passport import

--- app/lib/screens/passport_screen.dart
+++ app/lib/screens/passport_screen.dart
@@ -6,7 +6,6 @@ import 'package:provider/provider.dart';
 import '../data/daily_journey_catalog.dart';
 import '../state/app_state.dart';
 import '../theme/phoenix_theme.dart';
-import '../widgets/city_journey_stamp.dart';
 import '../widgets/journey_symbol_badge.dart';
 import '../widgets/journey_share_button.dart';
 import 'journey_screen.dart';
