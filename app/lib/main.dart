edc68b17fd50e8f695bc61c7530ae05de2c84a2b
dhv5bnmfmh-cmyk <dhv5bnmfmh@privaterelay.appleid.com>
2026-07-29 14:58:00 +0700

    Use local vocabulary timing only

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,18 +3,15 @@ import 'package:provider/provider.dart';
 
 import 'app.dart';
 import 'services/language_level_preference_store.dart';
-import 'services/vocabulary_popover_timer_policy.dart';
 import 'state/app_state.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await const LanguageLevelPreferenceStore().initializePhoenixLevel();
-  runWithPhoenixTimerPolicy(() {
-    runApp(
-      ChangeNotifierProvider(
-        create: (_) => AppState()..load(),
-        child: const PhoenixApp(),
-      ),
-    );
-  });
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AppState()..load(),
+      child: const PhoenixApp(),
+    ),
+  );
 }
