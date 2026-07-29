cc1131772468c333c083ac7b741e348c4e157440
dhv5bnmfmh-cmyk <dhv5bnmfmh@privaterelay.appleid.com>
2026-07-29 21:50:42 +0700

    test: cover eighteen adaptive journeys

--- app/test/all_journey_adaptive_levels_test.dart
+++ app/test/all_journey_adaptive_levels_test.dart
@@ -66,7 +66,7 @@ void main() {
         )
         .toList(growable: false);
 
-    expect(allJourneyExperiences, hasLength(15));
+    expect(allJourneyExperiences, hasLength(18));
     expect(
       levels.map((level) => level.wonderQuestion).toSet(),
       hasLength(allJourneyExperiences.length),
