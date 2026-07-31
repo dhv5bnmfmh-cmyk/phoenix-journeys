c6538b3d708016d1f644f7c14d28bf065b4222d9
dhv5bnmfmh-cmyk <dhv5bnmfmh@privaterelay.appleid.com>
2026-08-01 00:21:08 +0700

    refactor: split editorial actions from model and annotation export

--- app/lib/data/editorial_story_revision.dart
+++ app/lib/data/editorial_story_revision.dart
@@ -1,30 +1,9 @@
 import '../models/story_content.dart';
 import 'daily_journey_experience.dart';
+import 'editorial_story_revision_model.dart';
 import 'journey_data.dart';
 
-class EditorialStoryRevision {
-  const EditorialStoryRevision({
-    required this.id,
-    required this.protagonist,
-    required this.narrativeMode,
-    required this.emotionalArc,
-    required this.endingMode,
-    required this.sections,
-    required this.annotations,
-    required this.wonderQuestion,
-    required this.expressQuestion,
-  });
-
-  final String id;
-  final String protagonist;
-  final String narrativeMode;
-  final String emotionalArc;
-  final String endingMode;
-  final List<String> sections;
-  final List<ReadingAnnotation> annotations;
-  final String wonderQuestion;
-  final String expressQuestion;
-}
+export 'journey_data.dart' show ReadingAnnotation;
 
 DailyJourneyExperience applyEditorialStoryRevision(
   DailyJourneyExperience journey,
