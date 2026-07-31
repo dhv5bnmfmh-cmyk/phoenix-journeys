34d2b8df21a5eceb2a8a203f573510ec07c03103
dhv5bnmfmh-cmyk <dhv5bnmfmh@privaterelay.appleid.com>
2026-07-31 23:45:55 +0700

    feat: register complete Phoenix editorial story library

---
+++ app/lib/data/journey_editorial_revisions.dart
@@ -0,0 +1,14 @@
+import 'editorial_story_revision.dart';
+import 'ordinary_editorial_revisions_a.dart';
+import 'ordinary_editorial_revisions_b.dart';
+import 'ordinary_editorial_revisions_c.dart';
+import 'ordinary_editorial_revisions_d.dart';
+import 'special_editorial_revisions.dart';
+
+const editorialStoryRevisions = <String, EditorialStoryRevision>{
+  ...ordinaryEditorialRevisionsA,
+  ...ordinaryEditorialRevisionsB,
+  ...ordinaryEditorialRevisionsC,
+  ...ordinaryEditorialRevisionsD,
+  ...specialEditorialRevisions,
+};
