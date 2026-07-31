c43f5b60896ee3c09da2dc61109523cf6d8c81b2
dhv5bnmfmh-cmyk <dhv5bnmfmh@privaterelay.appleid.com>
2026-07-31 21:35:10 +0700

    test: update ordinary journey rotation count

--- app/test/seven_day_journey_catalog_test.dart
+++ app/test/seven_day_journey_catalog_test.dart
@@ -4,9 +4,9 @@ import 'package:phoenix_journeys/data/daily_journey_catalog.dart';
 void main() {
   test('reviewed destinations rotate without repeating during one cycle',
       () {
-    expect(dailyJourneyExperiences, hasLength(27));
+    expect(dailyJourneyExperiences, hasLength(32));
     expect(
-        dailyJourneyExperiences.map((item) => item.id).toSet(), hasLength(27));
+        dailyJourneyExperiences.map((item) => item.id).toSet(), hasLength(32));
 
     final cycle = List.generate(
       dailyJourneyExperiences.length,
