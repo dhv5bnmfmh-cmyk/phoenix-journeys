f1535e2df1450feb1c5da37bce99e81c4cad0c38
dhv5bnmfmh-cmyk <dhv5bnmfmh@privaterelay.appleid.com>
2026-07-31 09:31:29 +0700

    feat(journeys): route preview to five complete journeys

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'screens/coin_journey_screen.dart';
+import 'screens/five_journeys_screen.dart';
 import 'screens/shadowing_home_screen.dart';
 import 'theme/phoenix_theme.dart';
 import 'widgets/startup_gate.dart';
@@ -14,6 +15,7 @@ class PhoenixApp extends StatelessWidget {
     final prototype = uri.queryParameters['prototype'];
     final isPreviewHost = uri.host.startsWith('phoenix-journeys-pr-');
     final showShadowingHome = prototype == 'shadowing';
+    final showFiveJourneys = prototype == 'journeys';
     final showCoinJourney =
         prototype == 'coin-journey' ||
         (isPreviewHost && prototype == null && !showShadowingHome);
@@ -24,6 +26,8 @@ class PhoenixApp extends StatelessWidget {
       theme: PhoenixTheme.light,
       home: showShadowingHome
           ? const ShadowingHomeScreen()
+          : showFiveJourneys
+          ? const FiveJourneysScreen()
           : showCoinJourney
           ? const CoinJourneyPrototypeScreen()
           : const StartupGate(),
