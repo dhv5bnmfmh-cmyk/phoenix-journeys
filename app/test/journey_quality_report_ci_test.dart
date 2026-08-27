a0c8f543d14dcee3497d80bd3f33fd05d8de9b25
dhv5bnmfmh-cmyk <dhv5bnmfmh@privaterelay.appleid.com>
2026-08-27 09:27:24 +0700

    test: allow measured quality report runtime

--- app/test/journey_quality_report_ci_test.dart
+++ app/test/journey_quality_report_ci_test.dart
@@ -65,5 +65,5 @@ void main() {
     expect(report['approvedCount'], 360);
     expect(report['needsRevisionCount'], 0);
     expect(report['blockedCount'], 0);
-  }, timeout: const Timeout(Duration(minutes: 2)));
+  }, timeout: const Timeout(Duration(minutes: 3)));
 }
