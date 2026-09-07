e57c583b2e1a9daa7ea776536590d230e2fbc6f5
dhv5bnmfmh-cmyk <dhv5bnmfmh@privaterelay.appleid.com>
2026-09-07 19:46:48 +0700

    test(pr208): select Forbidden City by geographic label

--- .github/scripts/pr208_founder_milestone_probe.mjs
+++ .github/scripts/pr208_founder_milestone_probe.mjs
@@ -100,9 +100,9 @@ await find(page, '探索护照');
 await tap(page, '中国', { exact: true });
 await find(page, '请从左侧选择省份');
 await tap(page, '北京市', { exact: true });
-await tap(page, '紫禁城', { exact: true });
+await tap(page, '故宫博物院');
 await find(page, '选择 Story');
-await tap(page, '交接前的标记', { exact: true });
+await tap(page, '交接前的标记');
 await find(page, '1/5', { prefix: true });
 await find(page, '交接前的标记');
 
